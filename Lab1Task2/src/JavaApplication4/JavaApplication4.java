8d756e8060a7ef2156a4c5e5b1bce2d07aa14e32
Arapi Alena <Aul0na@rambler.ru>
2017-12-03 00:11:02 +0300

    Java1 refactoring, optimization.

--- Lab1Task2/src/JavaApplication4/JavaApplication4.java
+++ Lab1Task2/src/JavaApplication4/JavaApplication4.java
@@ -6,21 +6,35 @@ public class JavaApplication4
 {
     public static void main(String[] args) 
     {
-       
-        int year = 2017;
-        int dayNum = 4;
+        int year = 2000;
+        int dayNum = 300;
         int []daysInMonths = {31,28,31,30,31,30,31,31,30,31,30,31} ;
-        StringBuilder sb = new StringBuilder("");
-        String []months = {"январь","февраль","март","апрель",
-            "май","июнь", "июль", "август", "сентябрь", "октябрь", 
-            "ноябрь","декабрь"};
+
         int monthNum = 0;
-        boolean isLeapYear = ((year%4==0)&&(year%100!=0||year%400==0));
+        
+        boolean isLeapYear = ((year%4 == 0)&&(year%100 !=0 || year%400 == 0));
+        
         for(int days : daysInMonths)
         {
-            if(isLeapYear = true)days++;  
+            if(days == 28 && isLeapYear)days++;  
             
+            if(dayNum > days)
+            {
+                dayNum = dayNum - days;
+                monthNum++;
+            }    
         }     
-        System.out.println(days, monthNum);
+        
+         StringBuilder sb = new StringBuilder("");
+
+         String []months = {"январь","февраль","март","апрель",
+            "май","июнь", "июль", "август", "сентябрь", "октябрь", 
+            "ноябрь","декабрь"};
+         
+         sb.append(months[monthNum]);
+         sb.append(" ");
+         sb.append(dayNum);
+
+        System.out.println(sb);
     }
 }
